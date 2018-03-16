70d00fa99dad215f3ad4af5354754a6b831acac2
BrunoAwdd <bruno@awdd.com.br>
2018-03-16 11:24:23 -0300

    Update Checkup of documentation

--- app/services/expiration_service.rb
+++ app/services/expiration_service.rb
@@ -3,48 +3,76 @@ class ExpirationService
     @expiredDocs = 0
   end
 
-  def live
-    'i am live'
-  end
-
-  def expiredDocuments
-    expireds = Array.new
-    @documents = Credit::Client::Document.all
-    @documents.each do |document|
-      if (document.expired?)
-        @url = Rails.application.routes.url_helpers.url_for controller: '/credit/client/generals', action: 'show', id: 1, only_path: true
-        message = "A documentação do cliente <a href='#{@url}'> #{document.credit_client_general.brand} </a>, esta desatualizada. Os Arquivos "
-        @message = Usuario::Message.new(:message => message, :status => 0, :from_id => 3)
-        @message.save
-        expireds.push(@message )
-      end
-    end
-    return expireds
-  end
-
   def expiredDocs
     @clients = Credit::Client::General.all
+    ms = ''
     @clients.each do |client|
       @url = Rails.application.routes.url_helpers.url_for controller: '/credit/client/generals', action: 'show', id: client.id, only_path: true
-      subject = "A documentação do cliente <a href='#{@url}'> #{client.brand}</a>, esta desatualizada."
+      subject = "A documentação do cliente <a href='#{@url}'> #{client.brand}</a>, esta desatualizada:"
       message =  subject + '<br><ul>'
       client.documents.each do |document|
         if (document.expired?)
           @expiredDocs += 1
-          message += "<li>#{document.credit_document.name} esta expirado a #{document.expiredDays} dias </li>"
+          message += "<li>#{document.credit_document.name} esta expirado a #{document.expiredDays} dias; </li>"
         end
       end
+
+      message += expired_docs_partners (client.partners)
+      message += expired_docs_warranties (client.warranties)
       message += "</ul>"
+
       createMessage(message, subject)
+      ms = message
     end
+    ms
   end
 
   private
 
+    def expired_docs_partners (partners)
+      message = ''
+      partners.each do |partner|
+        if partner.contato.nil?
+          contato_nome = '(Informações do Sócio Pendente)'
+        else
+          contato_nome = partner.contato.full_name
+        end
+        message += "<li><strong>Sócio: </strong>#{contato_nome}</li><ul>"
+        partner.partner_documents.each do |document|
+          if (document.expired?)
+            @expiredDocs += 1
+            message += "<li>#{document.document_list.name} esta expirado a #{document.expiredDays} dias;"
+          end
+        end
+        message += '</ul>'
+      end
+      return message
+    end
+
+    def expired_docs_warranties (warranties)
+      message = ''
+      warranties.each do |warranty|
+        message += "<li><strong>Garantia: </strong>#{warranty.name}</li><ul>"
+        warranty.warranty_documents.each do |document|
+          if document.expired?
+            @expiredDocs += 1
+            message += "<li>#{document.document_list.name} esta expirado a #{document.expiredDays} dias;"
+          end
+        end
+        message += '</ul>'
+      end
+      return message
+    end
+
     def createMessage (message, short_message = nil)
       if (@expiredDocs > 0)
-          @message = Usuario::Message.new(:message => message, :status => 0, :from_id => 3, :subject => short_message)
-          @message.save
+        users = Usuario::General.all
+        @message = Usuario::Message.new(:message => message, :status => 0, :from_id => 3, :subject => short_message)
+        @message.save
+        users.each do |user|
+          user.messages_count =+1
+          user.update
+        end
       end
     end
 
