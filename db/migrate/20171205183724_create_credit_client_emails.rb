41417332e02f831c5a9497bd6c3590f4614f6eee
BrunoAwdd <bruno@awdd.com.br>
2018-03-16 11:29:03 -0300

    Update Migration

--- db/migrate/20171205183724_create_credit_client_emails.rb
+++ db/migrate/20171205183724_create_credit_client_emails.rb
@@ -3,7 +3,7 @@ class CreateCreditClientEmails < ActiveRecord::Migration[5.0]
     create_table :credit_client_emails do |t|
       t.string :email
       t.string :contact
-      t.references :credit_client_general, foreign_key: true
+      t.references :credit_client_general, foreign_key: { to_table: "credit_client_contact_bases"}
 
       t.timestamps
     end
