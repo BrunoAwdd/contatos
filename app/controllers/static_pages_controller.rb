e398670905c9468d317a7c795795e1c6701154fb
BrunoAwdd <bruno@awdd.com.br>
2018-05-07 10:44:28 -0300

    custom layout

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,15 @@
 class StaticPagesController < ApplicationController
   def home
+    if user_signed_in?
+      @credit_client_generals = Credit::Client::General.all.limit(5)
+      @business_internationals = Business::International.all.limit(5)
+      @business_nationals = Business::National.all.limit(5)
+      @business_sites = Business::Site.all.limit(5)
+    else
+      redirect_to new_user_session_path
+    end
+    #render :layout => 'application-fluid'
+
   end
 
   def about
